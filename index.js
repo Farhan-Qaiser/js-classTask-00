e8b0f4afa9a1a713815007ed07ea192b8583f33e
farhan1019 <stenierbrothers@gmail.com>
2024-02-21 16:08:37 +0500

    js-class-task-00 completed

---
+++ index.js
@@ -0,0 +1,51 @@
+//1: Sum of digits of a number
+//const number = +prompt("Enter a number:");
+//let sum = 0;
+//const numberString = number.toString();
+//for (let i = 0; i < numberString.length; i++) {
+//  const digit = +numberString[i];
+//  sum = sum + digit;
+//}
+//console.log("Sum of digits:", sum);
+
+
+//2: Sum of range of numbers
+//let num1 = +prompt("Enter 1st number")
+//let num2 = +prompt("Enter 2nd number")
+//let sum = 0
+//for (let i = num1; i <= num2; i++){
+//    sum = sum + i
+//}
+//console.log("Sum of range of numbers: " + sum);
+
+
+//3: Average of numbers
+//const numbers = [10, 20, 30, 40, 50];
+//let sum = 0;
+//let count = 0;
+//for (let i = 0; i < numbers.length; i++) {
+//  sum =sum + numbers[i];
+//  count++;
+//}
+//const average = sum / count;
+//console.log("Average:", average);
+
+  
+//4: Count of strings
+//let str = prompt("Enter 1st word")
+//let str2 = prompt("Enter 2nd string")
+//if (str.length === str2.length) {
+//    alert("They are equal")
+//}
+//else {
+//    alert("They are not equal")
+//}
+
+
+//5: Swapping letters
+//const str = prompt("Enter a string:");
+//const firstChar = str.slice(0, 1);
+//const lastChar = str.slice(-1);
+//const middleChar = str.slice(1, -1)
+//const newStr = lastChar + middleChar + firstChar;
+//console.log("Swapped string:", newStr);
